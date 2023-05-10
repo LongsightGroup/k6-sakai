da0acf667dfc4f5cca7221768a0306dcd623e8b8
Sam Ottenhoff <ottenhoff@longsight.com>
2023-05-10 15:42:02 -0400

    More virtual users

--- options.js
+++ options.js
@@ -2,13 +2,13 @@ export const options = {
     scenarios: {
       sakaiflood: {
         executor: 'shared-iterations',
-        vus: 1,
-        iterations: 10,
-        maxDuration: '30s',
+        vus: 700,
+        iterations: 100000,
+        maxDuration: '900s',
       },
     },
     thresholds: {
-      'http_req_duration': ['p(99)<4000'], // 99% of requests must complete below 3.0s
+      'http_req_duration': ['p(99)<4000'], // 99% of requests must complete below 4.0s
     },
     insecureSkipTLSVerify: true,
   };
